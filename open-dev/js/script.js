0980e62e0884b22c17ecd483cb4ec682d9270a12
capelosini <57864727+capelosini@users.noreply.github.com>
2021-03-30 18:05:47 -0300

    Add files via upload

--- open-dev/js/script.js
+++ open-dev/js/script.js
@@ -3,7 +3,8 @@ addEventListener("keyup", refresh)
 function refresh(){
     var codeHtml = document.getElementById('code-html').value;
     var codeCss = document.getElementById('code-css').value;
-    var code = (codeHtml + " " + "<style>" + codeCss + "</style>")
+    var codeJs = document.getElementById('code-js').value;
+    var code = ("<script>" + codeJs + "</script>" + codeHtml + " " + "<style>" + codeCss + "</style>")
     document.getElementById("responsive-page").innerHTML = code;
     console.log(code)
     return (code + " " + "<title>index</title>")
