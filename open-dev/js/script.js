c097de5dd69304f38b1435db16b1375c681d98de
capelosini <57864727+capelosini@users.noreply.github.com>
2021-04-01 22:18:17 -0300

    Add files via upload

--- open-dev/js/script.js
+++ open-dev/js/script.js
@@ -6,12 +6,14 @@ function refresh(){
     var codeJs = document.getElementById('code-js').value;
     var code = ("<script>" + codeJs + "</script>" + codeHtml + " " + "<style>" + codeCss + "</style>");
     document.getElementById("responsive-page").innerHTML = code;
-    return (code)
+    return (code);
 }
 
 function download(){
-    var newWindow = window.open('', '', 'left=0,top=0,width=1000,height=700,toolbar=0,scrollbars=0,status=0');
-    newWindow.document.write(refresh());
+    var newWindow = window.open('', '', 'left=0,top=0,width=1000,height=700,toolbar=0,scrollbars=0,status=0,resizable=yes');
+    var htmlCode = refresh();
+    newWindow.document.write(htmlCode);
     newWindow.focus();
+    console.log("<html>" + htmlCode + "</html>");
 }
   
