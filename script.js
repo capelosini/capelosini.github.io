b9a3c0c5bf65c6194b2f85c0adc863c5124bd0ee
Clóvis de Almeida Pelosini <capelosini@pop-os.localdomain>
2021-04-19 17:37:48 -0300

    New Style

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 window.addEventListener("scroll", function(){
     var value = window.scrollY;
     var title = document.getElementById("title-my-projects");
-    if (value <= 300){
-        title.style.paddingLeft = (value + "px");
-    }
+    title.style.paddingLeft = (value + "px");
 });
 
 function openDev(){
