f900ab434cad2717ece2aac2125eabb711db1802
Clovis de Almeida Pelosini <clovis@Cloviss-MacBook-Pro.local>
2021-04-30 03:08:24 -0300

    Update Site

--- open-dev/js/script.js
+++ open-dev/js/script.js
@@ -1,10 +1,56 @@
+const imgRocket = document.getElementById("rocket");
+const htmlCode = document.getElementById("code-html");
+const cssCode = document.getElementById("code-css");
+const jsCode = document.getElementById("code-js");
+const responsePage = document.getElementById("responsive-page");
+
 addEventListener("keyup", refresh)
 
+addEventListener("scroll", () => {
+    var s = window.scrollY;
+    var rh = responsePage.scrollHeight;
+    if (rh <= window.outerHeight){
+        responsePage.style.paddingTop = s + "px";
+    }
+    else{
+        responsePage.style.paddingTop = "0px";
+    }
+})
+
+imgRocket.addEventListener("click", download)
+
+htmlCode.addEventListener("keydown", (key) => {
+    if (key.key == "Tab"){
+        var cp = htmlCode.selectionStart;
+        htmlCode.value = htmlCode.value.substring(0, cp) + "    " + htmlCode.value.substring(cp, htmlCode.lenght);
+        setTimeout(() => { htmlCode.focus(); }, 50);
+        htmlCode.selectionStart = cp + 4;
+        htmlCode.selectionEnd = cp + 4;
+    }
+});
+
+cssCode.addEventListener("keydown", (key) => {
+    if (key.key == "Tab"){
+        var cp = cssCode.selectionStart;
+        cssCode.value = cssCode.value.substring(0, cp) + "    " + cssCode.value.substring(cp, cssCode.lenght);
+        setTimeout(() => { cssCode.focus(); }, 50);
+        cssCode.selectionStart = cp + 4;
+        cssCode.selectionEnd = cp + 4;
+    }
+});
+
+jsCode.addEventListener("keydown", (key) => {
+    if (key.key == "Tab"){
+        var cp = jsCode.selectionStart;
+        jsCode.value = jsCode.value.substring(0, cp) + "    " + jsCode.value.substring(cp, jsCode.lenght);
+        setTimeout(() => { jsCode.focus(); }, 50);
+        jsCode.selectionStart = cp + 4;
+        jsCode.selectionEnd = cp + 4;
+    }
+});
+
 function refresh(){
-    var codeHtml = document.getElementById('code-html').value;
-    var codeCss = document.getElementById('code-css').value;
-    var codeJs = document.getElementById('code-js').value;
-    var code = ("<script>" + codeJs + "</script>" + codeHtml + " " + "<style>" + codeCss + "</style>");
+    var code = ("<script>" + jsCode.value + "</script>" + htmlCode.value + " " + "<style>" + cssCode.value + "</style>");
     document.getElementById("responsive-page").innerHTML = code;
     return (code);
 }
