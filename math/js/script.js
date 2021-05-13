e7538e56295950ef583f2f814e2d152cf5faf1b2
Clóvis <capelosini@gmail.com>
2021-05-13 12:05:28 -0300

    New Design

--- math/js/script.js
+++ math/js/script.js
@@ -2,7 +2,7 @@ const Display = document.getElementById("Display");
 const Answer = document.getElementById("Answer");
 const gameResult = document.getElementById("gameResult");
 const Points = document.getElementById("Points");
-const maxLenght = 100;
+const maxLenght = 199;
 const numPoints = 25;
 var n1 = 0;
 var n2 = 0;
@@ -22,13 +22,13 @@ function submit(){
     result=n1+n2;
     if (result == Answer.value){
         gameResult.innerText = "Correct!";
-        gameResult.style.color = "green";
+        gameResult.style.color = "hsl(120, 100%, 60%)";
         points = points+numPoints;
         setTimeout(() => { game(); }, 2000);
     }
     else{
         gameResult.innerText = "Incorrect!     Result: " + result;
-        gameResult.style.color = "red";
+        gameResult.style.color = "hsl(0, 100%, 61%)";
         points=0
         setTimeout(() => { game(); }, 2000);
     };
