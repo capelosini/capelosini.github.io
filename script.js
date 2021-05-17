0c72c56268d27c5ccd93d31c7828ceb3942e8006
Clóvis <capelosini@gmail.com>
2021-05-17 12:36:26 -0300

    Update

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 window.addEventListener("scroll", function(){
     var value = window.scrollY;
     var title = document.getElementById("title-my-projects");
-    title.style.paddingLeft = (value + "px");
+    if (value <= 600){
+        title.style.paddingLeft = (value + "px");
+    }
 });
 
 function scrollDown(){
