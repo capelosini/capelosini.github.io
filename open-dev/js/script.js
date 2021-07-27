375cec956561e6673b6887d1fe402861d80f64b1
Clóvis <capelosini@gmail.com>
2021-07-27 17:55:58 -0300

    New Base HTML Text

--- open-dev/js/script.js
+++ open-dev/js/script.js
@@ -59,8 +59,18 @@ require(["vs/editor/editor.main"], function () {
     }
     addEventListener("keyup", refresh)
     imgRocket.addEventListener("click", download)
-    buttonBaseHtml.addEventListener("click", () => { htmlCode.setValue('<head>\n  <title>title</title>\n   <meta charset="utf-8">\n</head>\n<body>\n</body>') })
+    buttonBaseHtml.addEventListener("click", () => { htmlCode.setValue(`<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+      
+</body>
+</html>`) })
 });
-
 
   
