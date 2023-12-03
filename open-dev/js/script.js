cf1bf2266724f511e6f181904c7761922129e277
Clovis <capelosini@gmail.com>
2023-12-03 13:17:53 -0300

    Ajust code editor layout when resizing, open-dev

--- open-dev/js/script.js
+++ open-dev/js/script.js
@@ -47,6 +47,13 @@ require(["vs/editor/editor.main"], function () {
         language: 'javascript',
         theme: 'vs-dark'
     });
+
+    addEventListener("resize", () => {
+        htmlCode.layout();
+        cssCode.layout();
+        jsCode.layout();
+    })
+
     function refresh(){
         var code = ('<!-- Created with capelosini.github.io/open-dev -->\n\n<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">\n'+ htmlCode.getValue() + "\n<style>\n" + cssCode.getValue() + "\n</style>\n" + '<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>\n<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>\n<script>\n' + jsCode.getValue() + "\n</script>");
         var newBlob = new Blob([code], {type: "text/html"})
